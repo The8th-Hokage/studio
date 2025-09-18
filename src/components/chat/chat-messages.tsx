cfe3bcb1cc648cb7a649bd9d695b3d7a8568de08
Rich Guy <ncoin60@gmail.com>
2025-09-18 17:24:44 +0000

    Try fixing this error: `Console Error: React does not recognize the `vie

--- src/components/chat/chat-messages.tsx
+++ src/components/chat/chat-messages.tsx
@@ -21,7 +21,6 @@ export default function ChatMessages({
 }: {
   messages: MessageWithUser[];
 }) {
-  const scrollAreaRef = useRef<HTMLDivElement>(null);
   const viewportRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -31,7 +30,7 @@ export default function ChatMessages({
   }, [messages]);
 
   return (
-    <ScrollArea className="flex-1" ref={scrollAreaRef} viewportRef={viewportRef}>
+    <ScrollArea className="flex-1" viewportRef={viewportRef}>
       <div className="max-w-4xl mx-auto p-4 space-y-4">
         {messages.map((message) => (
           <ChatMessage key={message.id} message={message} />
